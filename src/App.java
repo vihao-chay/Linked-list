706d5f2ce5515910286db5ac1ebea2396eccc81b
chaykun <damvihao123@gmail.com>
2024-08-12 16:09:45 +0700

    CSD-Buoi1.1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("FunnyCode_Đàm Vĩ Hào");
+
+        System.out.println("BTVN Buoi 1");
     }
 }
